fa2a01d8ab650f21b4979c608ec9c83b492135d0
Ahum Maitra <theahummaitra@gmail.com>
2025-11-20 12:58:37 +0530

    Converted notes.txt format to json more readable format

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Input
 from textual.containers import ScrollableContainer
-import uuid
+import uuid  # to genrate id
+import json
+from datetime import datetime
+
 # All components
 from WelcomeTextRender import WelcomeText
 from AddNoteInputBox import UserNoteInputBox
@@ -12,29 +15,34 @@ from LogoText import LogoRender
 from VersionScreen import VersionScreen
 from ViewRawNotes import RawNotes
 
+
 class Itomori(App):
     CSS_PATH = "./style.tcss"
-    BINDINGS = [("^q", "quit", "Quit the app"), ("v", "show_ver", "Show version and info"), ("n", "show_row_notes", "View All Notes")]
+    BINDINGS = [
+        ("^q", "quit", "Quit the app"),
+        ("v", "show_ver", "Show version and info"),
+        ("n", "show_row_notes", "View All Notes"),
+    ]
 
     @on(Input.Submitted, "#NoteInputBox")
     def handle_tasks(self) -> None:
         user_typed_input = self.query_one("#NoteInputBox")
         self.user_note = user_typed_input.value.strip()
+        notes = {
+            "ID": str(uuid.uuid4()),
+            "Time": datetime.now().isoformat(),
+            "Task Text": self.user_note,
+        }
 
         # Write note to file
-        with open("notes.txt", "a") as notes:
-            notes.write("\n\n" + f"ID : {uuid.uuid4()}" + "Task Text : \n\n" + self.user_note + f"\tTime:{time.now()}\n")
-
-
+        with open("notes.json", "a") as notesfile:
+            notesfile.write(json.dumps(notes) + "\n")
 
     def compose(self) -> ComposeResult:
         yield Header()
 
         yield ScrollableContainer(
-            LogoRender,
-            WelcomeText,
-            WhereSavedWarn,
-            UserNoteInputBox
+            LogoRender, WelcomeText, WhereSavedWarn, UserNoteInputBox
         )
 
         yield Footer()
@@ -44,6 +52,7 @@ class Itomori(App):
 
     def action_show_row_notes(self) -> None:
         self.push_screen(RawNotes())
+
 
 if __name__ == "__main__":
     app = Itomori()

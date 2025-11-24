ae39f45bdcd219bc3f9ed0e57e8072cc81bb65e1
Ahum Maitra <theahummaitra@gmail.com>
2025-11-25 00:06:55 +0530

    improved the installation script

--- src/components/InfoWhereSaved.py
+++ src/components/InfoWhereSaved.py
@@ -5,4 +5,6 @@ from textual.widgets import Label
 This file is a component, which is just helps to give user a info where the notes are saved in.
 """
 
-WhereSavedWarn = Label("All Notes are saved in ../Itomori/src/notes.json", id="WhereSavedNotesWarn")
+WhereSavedWarn = Label(
+    "All Notes are saved in ../Itomori/src/notes.json", id="WhereSavedNotesWarn"
+)

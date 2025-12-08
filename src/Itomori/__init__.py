6ace2b6f711980751afb84bbeba2031ebfeea0a5
Ahum Maitra <theahummaitra@gmail.com>
2025-12-08 23:51:27 +0530

    Switched from mannual copyright notices to SPDX

--- src/Itomori/__init__.py
+++ src/Itomori/__init__.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2025-present Ahum Maitra theahummaitra@gmail.com
+#
+# SPDX-License-Identifier: 	GPL-3.0-or-later
+
+
 """
 This library helps to access all, components for creating Itomori.
 

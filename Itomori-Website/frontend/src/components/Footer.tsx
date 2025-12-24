4263de1a53ff7d8261c6a87aeaedbb32d16985a9
Ahum Maitra <theahummaitra@gmail.com>
2025-12-25 03:11:43 +0530

    enhancement : Improve website's Footer

--- Itomori-Website/frontend/src/components/Footer.tsx
+++ Itomori-Website/frontend/src/components/Footer.tsx
@@ -2,13 +2,16 @@
 
 //  SPDX-License-Identifier: 	GPL-3.0-or-later
 
+// import next js link component
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <>
+    {/* footer  */}
       <footer className="h-full w-full backdrop-blur-sm sticky bottom-0 mt-3 flex flex-col justify-center items-center font-bold p-3">
         <h1>
+          {/* inform visiter about - source code license and content license  */}
           This website&apos;s source code is open source and it&apos;s licensed under  <Link
             className="hover:italic hover:text-destructive ml-1"
             href={"https://www.gnu.org/licenses/gpl-3.0.en.html"}

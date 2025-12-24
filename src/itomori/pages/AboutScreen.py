876f47f165b5c273e162f2a49e99dc563625b0f0
Ahum Maitra <theahummaitra@gmail.com>
2025-12-24 23:20:33 +0530

    enhancement : Improve About page

--- src/itomori/pages/AboutScreen.py
+++ src/itomori/pages/AboutScreen.py
@@ -2,20 +2,23 @@
 #
 # SPDX-License-Identifier: 	GPL-3.0-or-later
 
-
-# all necessary Textual widgets, screens, containers
+# all necessary Textual subpackage, widgets, screens, containers
 from textual.screen import ModalScreen
 from textual.app import ComposeResult
 from textual.widgets import Label, Footer, Header
 from textual.containers import Container
+
+#import License modal screen component
 from itomori.components.LincenseScreen import LicenseScreen
+
+# import current Itomori version
 from itomori import __version__
 
 """
-This file is a component, which is just helps to render the version screen.
+About Page
 """
 
-
+#About modal screen
 class AboutScreen(ModalScreen[None]):
     """
     This class render the About Modal Screen.
@@ -26,40 +29,44 @@ class AboutScreen(ModalScreen[None]):
     # keyboard bindings for this modal screen
     BINDINGS = [("escape", "pop_screen", "Escape about screen"), ("l", "show_license", "Show License info")]
 
-    # css link
+    # set css file
     CSS_PATH: str = "../style.tcss"
 
     def compose(self) -> ComposeResult:
         """
-        Main Textaul compose function to render the About Modal Screen
+        Main Textual compose function to render the About Modal Screen
         """
-        yield Header(show_clock=True)
 
+        yield Header(show_clock=True) #show default header with user's local time
+
+        # main container for showing details
         with Container(id="AboutScreen"):
             """
             Main container for the Version Modal Screen
             """
-            # All labels
-            yield Label(f"[b]Itomori v{__version__}[/b]")  # Itmori current version
+            yield Label(f"[b]Itomori v{__version__}[/b]")  # Show Itomori's current version
             yield Label(
-                "[italic bold]Author : Ahum Maitra[italic bold]"
-            )  # My name as author
+                "[italic bold]Creator : Ahum Maitra[italic bold]"
+            )  # My name as creator
 
-            # Github link label to show the github link
+            #Show Github repository link
             yield Label(
                 "[yellow bold]Github link : [underline]https://github.com/TheAhumMaitra/Itomori[/underline][yellow bold]"
             )
 
+            # inform user, how to see `GPL-3.0-or-later` full license text
             yield Label("[b underline green]Press `L` to view License info[/b underline green]")
+
+        #show footer
         yield Footer()
 
+    # if user requested exit About modal screen
     def action_pop_screen(self) -> None:
         """
         This Textual action method helps to exit the modal screen by pressing 'ESC'
         """
         self.dismiss()  # if the action triggered then dismiss the screen
 
+    # if user requested to see full license text
     def action_show_license(self):
-        self.app.push_screen(LicenseScreen())
-
-
+        self.app.push_screen(LicenseScreen()) #then push `License` screen

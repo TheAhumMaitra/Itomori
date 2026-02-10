93535a74da838cba6f0c0095a0c759a117b52ba1
Ahum Maitra <theahummaitra@gmail.com>
2026-02-10 21:37:04 +0530

    enhancement: NOw the installtion methods are more cleaner

--- Itomori-Website/frontend/src/components/Installation.tsx
+++ Itomori-Website/frontend/src/components/Installation.tsx
@@ -7,9 +7,11 @@
 import { CopyButton } from "./ui/shadcn-io/copy-button";
 
 export default function Installation() {
-  let uv_url = "uv tool install git+https://github.com/TheAhumMaitra/Itomori.git";
+  let uv_Git_url = "uv tool install git+https://github.com/TheAhumMaitra/Itomori.git";
 
-  let pip_url = "pip install Itomori";
+  let uv_package_url = "uv tool install Itomori"
+
+  let pipx_url = "pipx install Itomori";
 
   return (
     <>
@@ -18,27 +20,36 @@ export default function Installation() {
 
     <div className="h-[50vh] flex flex-col justify-center gap-3 items-center mt-4 border-accent border-8 rounded-3xl">
       <h2 className="text-2xl font-bold underline font-mono!">
-        Install using uv in Windows or Linux or Mac OS or others:
+        Install Itomori using uv
       </h2>
+      <h3 className="text-chart-2 font-medium text-sm">Available for Windows, Linux, Mac OS</h3>
 
       <div className="text-1xl p-3 font-bold cursor-pointer! flex gap-3">
         <span className="p-3 rounded-3xl bg-card flex gap-3 text-center justify-center items-center">
-        <h3>{uv_url}</h3>
-        <CopyButton content={uv_url} variant="default" size="md" />
+        <h3>{uv_Git_url}</h3>
+        <CopyButton content={uv_Git_url} variant="default" size="md" />
         </span>
 
+
       </div>
+        <h3>Or (use the package) <span className="text-chart-4">Which is more stable</span></h3>
+        <span className="p-3 rounded-3xl bg-card flex gap-3 text-center justify-center items-center">
+        <h3>{uv_package_url}</h3>
+        <CopyButton content={uv_package_url} variant="default" size="md" />
+        </span>
+
     </div>
 
     <div className="h-[50vh] rounded-3xl flex flex-col justify-center gap-3 items-center mt-4 border-accent border-8">
       <h2 className="text-2xl font-bold underline font-mono!">
-        Install using pip in Windows or Linux or Mac OS or others:
+        Install using pipx
       </h2>
+      <h3 className="text-chart-2 font-medium text-sm">Available for Windows, Linux, Mac OS</h3>
 
       <div className="text-1xl p-3 font-bold cursor-pointer! flex gap-3">
         <span className="p-3 rounded-3xl bg-card flex gap-3 text-center justify-center items-center">
-        <h3>{pip_url}</h3>
-        <CopyButton content={pip_url} variant="default" size="md" />
+        <h3>{pipx_url}</h3>
+        <CopyButton content={pipx_url} variant="default" size="md" />
         </span>
 
       </div>
